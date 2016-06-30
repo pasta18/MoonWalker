aa108a0cea33662b99eb58d3bd72a75e30947072
pasta18 <lightsworn449black@gmal.com>
2016-06-30 16:53:49 +0900

    一連の流れを作った
    シーン推移、スコア、敵のランダム生成を実装しゲームとして動くようにしました。

---
+++ Assets/Scripts/PointController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class PointController
+{
+    static int point = 0;
+	// pointの取得
+    public static int Point
+    {
+        get { return point; }
+    }
+    // pointは加算のみ可能
+    public static void Add(int addPoint)
+    {
+        point += addPoint;
+    }
+    // pointの初期値化
+    public static void Reset()
+    {
+        point = 0;
+    }
+}
---
+++ Assets/Scripts/ResultScene.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ResultScene : MonoBehaviour
+{
+	void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            SceneManager.LoadScene("TitleScene");
+        }
+	}
+}
